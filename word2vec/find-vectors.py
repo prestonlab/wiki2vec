16d13fb8e27c7622f8a27ddf61d1533eed1ac6a2
Neal Morton <mortonne@gmail.com>
2019-11-25 14:20:33 -0600

    update script interface, add handling of spaces

--- word2vec/find-vectors.py
+++ word2vec/find-vectors.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python
 
-import sys
+import argparse
 
 from wiki2vec import word2vec
 
-items_file = sys.argv[1]
-vectors_file = sys.argv[2]
-out_file = sys.argv[3]
 
-# read items to look up
-with open(items_file, 'r') as f:
-    items = [l.strip() for l in f]
+def main(items_file, vectors_file, out_file):
+    # read items to look up
+    with open(items_file, 'r') as f:
+        items = [l.strip().replace(' ', '_') for l in f]
 
-vectors = word2vec.find_vectors(items, vectors_file)
+    # find each vector in the vectors file
+    vectors = word2vec.find_vectors(items, vectors_file)
 
-# write out the vectors with labels
-with open(out_file, 'w') as f:
-    for i, item in enumerate(items):
-        f.write('{} '.format(item))
-        for j in range(vectors.shape[1]):
-            f.write('{:.6f} '.format(vectors[i, j]))
-        f.write('\n')
+    # write out the vectors with labels
+    with open(out_file, 'w') as f:
+        for i, item in enumerate(items):
+            f.write('{} '.format(item))
+            for j in range(vectors.shape[1]):
+                f.write('{:.6f}'.format(vectors[i, j]))
+                if j < (vectors.shape[1] - 1):
+                    f.write(' ')
+            f.write('\n')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Get vectors for a list of items.")
+    parser.add_argument("items", help="Path to file with one item per line.")
+    parser.add_argument("vectors", help="Path to vectors file.")
+    parser.add_argument("output", help="Path to item vectors output file.")
+    args = parser.parse_args()
+
+    main(args.items, args.vectors, args.output)
