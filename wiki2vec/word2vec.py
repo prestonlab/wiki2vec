65133378ae52f848840f8ff02dea093122f87d28
Neal Morton <mortonne@gmail.com>
2019-11-25 13:29:07 -0600

    faster version of find_vectors with handling of unicode errors

--- wiki2vec/word2vec.py
+++ wiki2vec/word2vec.py
@@ -1,6 +1,19 @@
 """Utilities for working with word2vec vectors."""
 
+import warnings
 import numpy as np
+
+
+def read_lines(f):
+    """Read lines from a file while checking for unicode problems."""
+    while True:
+        try:
+            yield(next(f))
+        except StopIteration:
+            break
+        except UnicodeDecodeError as e:
+            warnings.warn('Unicode decoding problem.', UnicodeWarning)
+            pass
 
 
 def find_vectors(items, vectors_file):
@@ -10,7 +23,7 @@ def find_vectors(items, vectors_file):
     ----------
     items : list of strings
         List of terms to get vectors for.
-    
+
     vectors_file : string
         Path to a vectors file. In each line of the file, the first
         column is the term, and the remaining columns give the vector.
@@ -22,12 +35,26 @@ def find_vectors(items, vectors_file):
         as items.
     """
 
-    # build an index of lines and terms
-    print("Building index of vectors...")
-    vf = VectorFile(vectors_file)
+    item_set = set(items)
+    with open(vectors_file, 'r', encoding='utf-8') as f:
+        header_line = f.readline()
+        n_dim = int(header_line.split()[1])
+        vectors = np.empty((len(items), n_dim))
+        vectors.fill(np.nan)
+        for line in read_lines(f):
+            # get the term for this vector
+            term = line.split()[0]
+            if term in item_set:
+                # if the term matches anything in the item set, store the vector
+                x = np.array(line.split()[1:(n_dim + 1)], dtype=float)
+                vectors[items.index(term)] = x
 
-    print("Searching for items...")
-    vectors = vf.get_item_list(items)
+                # remove that term so we won't waste time checking for it
+                item_set.remove(term)
+
+                if not item_set:
+                    # no more items to look up
+                    break
     return vectors
 
 
